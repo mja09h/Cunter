9dd3a6379525882f8b88477a1bcc4b2e239a27ed
mja09h <mja09h@gmail.com>
2025-09-21 14:36:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let counter = 0;
 
+const count = () => {
+  counter++;
+  document.getElementById("dis").innerHTML = counter;
+  if (counter % 100 === 0) {
+    alert(`Count is now ${counter}`);
+  }};
+
+document.addEventListener("DOMContentLoaded", functhion() {
+  document.getElementById("btn-count").onclick = count;
+});
